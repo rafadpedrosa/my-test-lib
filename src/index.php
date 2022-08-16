2139a3b90f105be51773ca4875b7464e26906136
rafadpedrosa <rafadpedrosa@gmail.com>
2022-08-15 21:25:21 -0300

    second commit with code

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace rafadpedrosa;
+
+
+class Test {
+    public function execute() {
+        echo "method executed";
+        return "success";
+    }
+}
+
+echo "carregou o arquivo";
